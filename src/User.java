51b2995e3fc396756df57cb2ab69abafe575e29b
Cosmin Alex <silvian1995@yahoo.com>
2021-03-16 19:27:55 +0200

    New class

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
+
+}
